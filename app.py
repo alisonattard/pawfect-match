a28dab8977821b49bb82619c93c7fddec5cffe27
alisonattard <alison12.attard@gmail.com>
2024-11-21 13:55:46 +0100

    customer css

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return app.send_static_file('home.html')
 
+@app.route('/customer')
+def customer():
+    return app.send_static_file('customer.html')
+
 # Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
